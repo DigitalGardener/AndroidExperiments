4a80a41a7fd9fde5728ea71c04e1ceb1b1601911
DigitalGardener <DigitalGardener@users.noreply.github.com>
2014-10-27 16:46:16 -0500

    Added code to calculate and display total bill  when bill without tip entered

--- TipCalculator/src/com/example/tipcalculator/MainActivity.java
+++ TipCalculator/src/com/example/tipcalculator/MainActivity.java
@@ -2,15 +2,55 @@ package com.example.tipcalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	private EditText totalBillEditText;
+	private EditText tipEditText;
+	private EditText billWithoutTipEditText;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		billWithoutTipEditText = (EditText)findViewById(R.id.billWithoutTipEditText);
+		billWithoutTipEditText.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable arg0) {
+				
+				double tip = Double.parseDouble(tipEditText.getText().toString());
+				double billWithoutTip = Double.parseDouble(billWithoutTipEditText.getText().toString());
+				
+				double totalBill = billWithoutTip * (1 +  tip);
+				
+				totalBillEditText.setText(String.format("%5.2f", totalBill));
+				
+			}
+		});
+		tipEditText = (EditText) findViewById(R.id.tipEditText);
+		totalBillEditText = (EditText) findViewById(R.id.totalBillEditText);
 	}
 
 	@Override
